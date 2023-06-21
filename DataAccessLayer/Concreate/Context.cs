6044f6e7e872af00e06a0fd464a3c4b0694e8082
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-06-21 20:43:28 +0300

    Add project files.

---
+++ DataAccessLayer/Concreate/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concreate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concreate
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JNJNVEQ\\MERTSQL;initial catalog=UowDb;integrated security=true");
+
+        }
+
+        public DbSet<Customer> Customers    { get; set; }
+        public DbSet<Process> Processes    { get; set; }
+    }
+}
---
+++ DataAccessLayer/EntityFramework/EfProcessDal.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concreate;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfProcessDal : GenericRepository<Process>,IProcessDal
+    {
+        public EfProcessDal(Context context) : base(context)
+        {
+
+        }
+    }
+}
